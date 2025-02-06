a5a9688651c6681072ef56c08244224987ae9b56
Fran Vañó <36304422+franvf@users.noreply.github.com>
2025-02-06 15:31:54 +0100

    fix: entrypoint (#8)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
 
-# Check if Ponder is already running
-EXISTING_TASKS=$(aws ecs list-tasks --cluster orp-cluster --service-name orp-service-beta | jq -r '.taskArns | length')
+echo "Attempting to start Ponder..."
 
-if [ "$EXISTING_TASKS" -eq 0 ]; then
-    echo "Starting Ponder in primary mode..."
-    npx ponder start
+if npx ponder start; then
+    echo "Ponder started successfully."
 else
-    echo "Starting Ponder in secondary mode..."
+    echo "Ponder start failed. Running 'npx ponder serve' instead..."
     npx ponder serve
 fi
