cc81f7362ae9905f56f435a615abd89026169f91
Fran Vañó <36304422+franvf@users.noreply.github.com>
2025-02-06 13:26:19 +0100

    fix: add entrypoint to avoid run ponder start twice (#6)

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Check if Ponder is already running
+EXISTING_TASKS=$(aws ecs list-tasks --cluster orp-cluster --service-name orp-service-beta | jq -r '.taskArns | length')
+
+if [ "$EXISTING_TASKS" -eq 0 ]; then
+    echo "Starting Ponder in primary mode..."
+    npx ponder start
+else
+    echo "Starting Ponder in secondary mode..."
+    npx ponder serve
+fi
