1ead68fcacfa1fe31760983c04a4f9cf3c1d5a45
Fran Vañó <36304422+franvf@users.noreply.github.com>
2025-02-19 09:57:22 +0100

    fix: script to reset the db before connecting again (#13)

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "$DEPLOY_ENV"
+
+if [[ "$DEPLOY_ENV" == "beta" ]]; then
+    echo "Installing PostgreSQL client..."
+    apt-get update && apt-get install -y postgresql-client
+
+    echo "Resetting database..."
+    psql $POSTGRES_SCHEMA -c "DROP DATABASE beta_orp WITH (FORCE); CREATE DATABASE beta_orp;"
+    echo "Db reset successfully"
+else 
+    echo "Not in beta env"
+fi
+
+echo "Starting application..."
+exec "$@"
