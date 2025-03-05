4696ae3fc39a021a2f847ab4cdc9008d70e1118f
Fran Vañó <franvfxi@gmail.com>
2025-03-05 10:43:37 +0100

    fix date format

--- src/web3/utils.ts
+++ src/web3/utils.ts
@@ -15,14 +15,6 @@ export async function signerConnection(signerPrivateKey: string) {
 }
 
 export function timestampToString(time: BigInt) {
-  const date = new Date(Number(time) * 1000);
-
-  const day = String(date.getUTCDate()).padStart(2, '0');
-  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-  const year = date.getUTCFullYear();
-  const hours = String(date.getUTCHours()).padStart(2, '0');
-  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-
-  return `${day}/${month}/${year} ${hours}:${minutes}`;
-
+  const date = new Date(Number(time)*1000);
+  return date.toISOString();
 }
